5f1d1a960967f83f088daea716b1dfd7986409a8
osamikoyo <v.pylayev@mail.ru>
2025-02-09 09:06:31 +0500

    final crud

--- internal/data/data.go
+++ internal/data/data.go
@@ -24,3 +24,26 @@ func (s *Storage) Create(worker *models.Worker) (uint64, error) {
 	err := s.db.Create(worker).Error
 	return worker.UserID, err
 }
+
+func (s *Storage) Update(id uint64, worker *models.Worker) error {
+	result := s.db.Model(&models.Worker{}).Where(&models.Worker{
+		UserID: id,
+	}).Updates(worker)
+	return result.Error
+}
+
+func (s *Storage) Delete(id uint64) error {
+	result := s.db.Where(&models.Worker{
+		UserID: id,
+	}).Delete(&models.Worker{})
+	return result.Error
+}
+
+func (s *Storage) Get(id uint64) (*models.Worker, error) {
+	var worker models.Worker
+	
+	result := s.db.Where(&models.Worker{
+		UserID: id,
+	}).Find(&worker)
+	return &worker, result.Error
+}
