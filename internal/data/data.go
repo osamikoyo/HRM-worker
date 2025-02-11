61e6fd04db2c26af3b0a0fa9178890777080819c
osamikoyo <v.pylayev@mail.ru>
2025-02-11 12:56:45 +0500

    update func

--- internal/data/data.go
+++ internal/data/data.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"database/sql"
+	"fmt"
+
 	"github.com/osamikoyo/hrm-worker/internal/config"
 	"github.com/osamikoyo/hrm-worker/internal/data/models"
 )
@@ -35,30 +37,76 @@ func InitStorage(cfg *config.Config) (*Storage, error) {
 }
 
 func (s *Storage) Create(worker *models.Worker) (uint64, error) {
-	_, err := s.db.Query("INSERT INTO workers (Firstname, Secondname, Salary, Email, Post) values (?1, ?2, ?3, ?4, ?5)",
+	_, err := s.db.Query("INSERT INTO workers (Firstname, Secondname, Salary, Email, Post) values (?, ?, ?, ?, ?)",
 	worker.Firstname, worker.Secondname, worker.Salary, worker.Email, worker.Post)
 	return worker.UserID, err
 }
 
 func (s *Storage) Update(id uint64, worker *models.Worker) error {
-	result := s.db.Model(&models.Worker{}).Where(&models.Worker{
-		UserID: id,
-	}).Updates(worker)
-	return result.Error
+	query := `
+        UPDATE workers
+        SET Firstname = ?, Secondname = ?, Salary = ?, Email = ?, Post = ?
+        WHERE UserID = ?
+    `
+	result, err := s.db.Exec(query, worker.Firstname, worker.Secondname, worker.Salary, worker.Email, worker.Post, id)
+    if err != nil {
+        return fmt.Errorf("error updating worker: %w", err)
+    }
+	affected, err := result.RowsAffected()
+	if err != nil{
+		return fmt.Errorf("error getting affected: %w", err)
+	}
+
+	if affected == 0 {
+		return fmt.Errorf("no worker found with id = %d", id)
+	}
+
+	return nil
 }
 
 func (s *Storage) Delete(id uint64) error {
-	result := s.db.Where(&models.Worker{
-		UserID: id,
-	}).Delete(&models.Worker{})
-	return result.Error
+	query := `
+	DELETE FROM workers
+    WHERE UserID = ?
+	`
+
+	result, err := s.db.Exec(query, id)
+	if err != nil{
+		return fmt.Errorf("error deleting worker id: %d, error: %w", id, err)
+	}
+
+	affected, err := result.RowsAffected()
+	if err != nil{
+		return fmt.Errorf("error get affected: %w", err)
+	}
+
+	if affected == 0 {
+		return fmt.Errorf("worker not found")
+	}
+
+	return nil
 }
 
 func (s *Storage) Get(id uint64) (*models.Worker, error) {
+	query := `
+		SELECT FROM workers
+		WHERE UserID = ?
+	`
+
+	result, err := s.db.Query(query, id)
+	if err != nil{
+		return nil, fmt.Errorf("error get, id: %d, error: %w", id, err)
+	}
+
 	var worker models.Worker
-	
-	result := s.db.Where(&models.Worker{
-		UserID: id,
-	}).Find(&worker)
-	return &worker, result.Error
+	for result.Next() {
+		result.Scan(&worker.UserID, 
+			&worker.Firstname, 
+			&worker.Secondname,
+			&worker.Salary,
+			&worker.Email,
+			&worker.Post)
+	}
+
+	return &worker, nil
 }
